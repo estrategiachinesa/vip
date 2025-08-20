10064852e3fe2f96ad67564d06513dcaeb5252a6
Trader Chines <traderchinesvip@gmail.com>
2025-08-20 22:23:18 +0000

    prepare o projeto para que ele seja subido no github pages, usando githu

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type {NextConfig} from 'next';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  // IMPORTANT: Replace <your-repo-name> with the name of your GitHub repository.
+  basePath: repoName ? `/${repoName}` : '',
+  assetPrefix: repoName ? `/${repoName}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
