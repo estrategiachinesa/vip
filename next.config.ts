1ad71382e63cd281acf7299bcdb0842d5f608b80
Estratégia Chinesa <estrategiachinesa@gmail.com>
2025-10-23 23:43:04 +0000

    fiz o deploy mas a apagina ficou assim

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type {NextConfig} from 'next';
 
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
+const repoName = 'vip';
 
 const nextConfig: NextConfig = {
   /* config options here */
